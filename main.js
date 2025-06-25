0fe32f7805df0d8b9e14269805235213adf178d5
Eniafe80 <oluwadamilola4357@gmail.com>
2025-06-25 23:14:31 +0100

    main.js

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-let slides = document.querySelectorAll('.slide');
-let current = 0;
-function nextSlide(){
-  slides[current].classList.remove('active');
-  current = (current + 1) % slides.length;
-  slides[current].classList.add('active');
-}
-setInterval(nextSlide, 4000);
 const slider = document.getElementById("heroSlider");
+
 const images = [
   "placeholder1.jpg",
   "placeholder2.jpg",
   "placeholder3.jpg"
 ];
+
 let current = 0;
 
 function changeSlide() {
