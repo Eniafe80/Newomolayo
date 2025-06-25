71201e82fdcbc57058ff0c8b53ff746858bfc178
Eniafe80 <oluwadamilola4357@gmail.com>
2025-06-25 23:06:00 +0100

    main.js

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function nextSlide(){
   slides[current].classList.add('active');
 }
 setInterval(nextSlide, 4000);
+const slider = document.getElementById("heroSlider");
+const images = [
+  "placeholder1.jpg",
+  "placeholder2.jpg",
+  "placeholder3.jpg"
+];
+let current = 0;
+
+function changeSlide() {
+  current = (current + 1) % images.length;
+  slider.style.backgroundImage = `url(${images[current]})`;
+}
+
+slider.style.backgroundImage = `url(${images[0]})`;
+setInterval(changeSlide, 4000);
